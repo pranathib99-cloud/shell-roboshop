7936d0a7daf1cbd13643d54757fdcc0d853f4796
pranathi.b99-cloud <pranathi.b99@gmail.com>
2025-10-09 17:48:28 +0530

    shell-robo-s17

---
+++ roboshop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0b5711d20e4247352"
+
+for instance in $@
+do 
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0b5711d20e4247352 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    
+    #get private or public ip based on frentend{public} or backend{private}
+    if  [ $instance ! = "frontend " ];then
+        IP=$(aws ec2 describe-instances --instance-ids i-06afedc2354d816fc --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids i-06afedc2354d816fc --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+    fi
+    echo "$instance  $IP"
+
+done
