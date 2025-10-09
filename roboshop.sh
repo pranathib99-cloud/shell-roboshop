f9efff4c4291951f17745eb760701a644a78e337
pranathi.b99-cloud <pranathi.b99@gmail.com>
2025-10-09 18:27:35 +0530

    shell-robo-s17

--- roboshop.sh
+++ roboshop.sh
@@ -3,7 +3,7 @@
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0b5711d20e4247352"
 
-for instance in $@
+for instance in "$@" ;
 do 
   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0b5711d20e4247352 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
     
