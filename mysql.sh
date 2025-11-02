b039fd30015697ffa6ba56901a2cc5bb4d576910
pranathi.b99-cloud <pranathi.b99@gmail.com>
2025-11-02 11:59:21 +0530

    section 19

---
+++ mysql.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERID=$(id -u)     #get user id of current user
+R="\e[31m"  #Red
+G="\e[32m" #Green
+Y="\e[33m"] #Yellow
+N="\e[0m"  #No Color white
+LOGS_FLODER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.zyna.space
+LOGS_FILE="$LOGS_FLODER/$SCRIPT_NAME.log"
+
+mkdir -p "$LOGS_FLODER"
+echo "script started at : $(date)"  | tee -a $LOGS_FILE
+
+if [ $USERID -ne 0 ]; then 
+    echo " ERROR:: please run this script as root privileges"
+    exit 1          #failure is other then 0
+fi 
+
+VALIDATE(){                                        #Functions recevive input to /Aguments just like scripts arguments
+    if [ $1 -ne 0 ]; then                          # $1 is exit status of last command
+        echo -e "$2 .. $R failure $N"
+        exit 1                                     #exit with failure status 
+    else
+        echo -e "$2 ..$G success $N"
+    fi
+}
+
+####mysql installation #####
+
+dnf install mysql-server -y
+VALIDATE $? "installing mysql server"
+
+systemctl enable mysqld
+VALIDATE $? "enabling mysql service"
+
+systemctl start mysqld  
+VALIDATE $? "starting mysql service"
+
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? " Setting mysql root password"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME -$START_TIME ))
+
+echo -e " script executed in : $Y $TOTAL_TIME seconds $N"
