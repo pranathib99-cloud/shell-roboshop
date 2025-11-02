0ef739e3b6f3c45b3dff2622cfd020281e6b0efe
pranathi.b99-cloud <pranathi.b99@gmail.com>
2025-11-02 10:23:22 +0530

    section 19

---
+++ redis.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+USERID=$(id -u)     #get user id of current user
+R="\e[31m"  #Red
+G="\e[32m" #Green
+Y="\e[33m"] #Yellow
+N="\e[0m"  #No Color white
+LOGS_FLODER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.zyna.space
+LOGS_FILE="$LOGS_FLODER/$SCRIPT_NAME.log"
+
+mkdir -p "$LOGS_FLODER"
+echo "script started at : $(date)"  | tee -a $LOGS_FILE
+
+if [ $USERID -ne 0 ]; then 
+    echo " ERROR:: please run this script as root privileges"
+    exit 1          #failure is other then 0
+fi 
+
+VALIDATE(){                                        #Functions recevive input to /Aguments just like scripts arguments
+    if [ $1 -ne 0 ]; then                          # $1 is exit status of last command
+        echo -e "$2 .. $R failure $N"
+        exit 1                                     #exit with failure status 
+    else
+        echo -e "$2 ..$G success $N"
+    fi
+}
+
+#############
+dnf module disable redis -y
+VALIDATE $? "disabling defaut redise"
+
+dnf module enable redis:7 -y
+VALIDATE $? "enabling redis 7"
+
+dnf install redis -y
+VALIDATE $? "Installing redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e 's/protected-mode c protected-mode no/' /etc/redis/redis.conf 
+#-i permanent edit file and -e for multiple expressions and c is for change
+VALIDATE $? "updating redis listen address"
+
+systemctl enable redis 
+VALIDATE $? "enabling redis service"
+
+systemctl start redis
+VALIDATE $? "starting redis service" 
+
+#############
+ #-i permanent edit file and -e for multiple expressions and c is for change
