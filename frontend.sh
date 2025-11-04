bb61fd136912e806c58bb499129a0d0535e60cc6
pranathi.b99-cloud <pranathi.b99@gmail.com>
2025-11-04 06:31:40 +0530

    18 s

---
+++ frontend.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+#frontend and catalogue are same
+
+USERID=$(id -u)     #get user id of current user
+R="\e[31m"  #Red
+G="\e[32m" #Green
+Y="\e[33m"] #Yellow
+N="\e[0m"  #No Color white
+LOGS_FLODER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.zyna.space
+LOGS_FILE="$LOGS_FLODER/$SCRIPT_NAME.log"
+
+mkdir -p "$LOGS_FLODER"
+echo "script started at : $(date)"  | tee -a $LOGS_FILE
+
+if [ $USERID -ne 0 ]; then 
+    echo " ERROR:: please run this script as root privileges"
+    exit 1          #failure is other then 0
+fi 
+
+VALIDATE(){                                        #Functions recevive input to /Aguments just like scripts arguments
+    if [ $1 -ne 0 ]; then                          # $1 is exit status of last command
+        echo -e "$2 .. $R failure $N"
+        exit 1                                     #exit with failure status 
+    else
+        echo -e "$2 ..$G success $N"
+    fi
+}
+
+#####nodejs installation #####
+
+
+
+dnf module disable nginx -y &>>$LOGS_FILE
+dnf module enable nginx:1.24 -y &>>$LOGS_FILE
+
+dnf install nginx -y &>>$LOGS_FILE
+systemctl enable nginx  &>>$LOGS_FILE
+
+systemctl start nginx curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOGS_FILE
+
+cd /usr/share/nginx/html &>>$LOGS_FILE
+unzip /tmp/frontend.zip&>>$LOGS_FILE
+
+rm -rf /etc/nginx/nginx.conf  &>>$LOGS_FILE
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOGS_FILE
+
+
+
+systemctl restart nginx &>>$LOGS_FILE
+
+
+
+VALIDATE $? "frontend setup completed"
+
+q
+
+
+
+
+
+
+
+
+
+#end of script
+# netstat -ltnp #to check listening ports
+# curl http:/localhost:8080/health 
